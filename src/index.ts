e8cd0783590e28e410951b622a287ee3842ad715
Andrew Butt <Pryme8@gmail.com>
2024-02-16 16:33:09 -0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,36 +2,7 @@ import { ApiRequest, ApiRoutes } from "./api/api";
 import "./css/main.css";
 import { ChatBox, ChatBoxMessageType } from "./layouts/chatBox";
 
-
-// 
-// 
-// 
-// 
-// import { createRetrievalChain } from "langchain/chains/retrieval";
-// import { createHistoryAwareRetriever } from "langchain/chains/history_aware_retriever";
-// 
-// 
-
-
-
-
-// import { ApiRequest, ApiRoutes } from "./api/api";
-
-
-
 const appRoot = document.getElementById('app');
-
-
-
-
-// const historyAwarePrompt = ChatPromptTemplate.fromMessages([
-//     new MessagesPlaceholder("chat_history"),
-//     ["user", "{input}"],
-//     [
-//       "user",
-//       "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation",
-//     ],
-//  ]);
 
 if(appRoot){    
     const chatBox = new ChatBox(appRoot);    
